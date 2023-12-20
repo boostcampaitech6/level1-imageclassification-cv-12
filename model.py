df05ba2165283e5a3a5310e3ceb490dd56c1c4f8
백광현_T6074 <118159810+bkkhyunn@users.noreply.github.com>
2023-12-20 10:36:04 +0900

    [FEAT] ✨ EfficientViT 모델 추가 및 잔 코드 업데이트 ✨  (#51)
    * [feat] add data process file
    
    add fake image(over age 57) using MixUp
    
    #36
    
    * [FEAT] 12.19 코드 업데이트
    
    - 실험을 위해 전반적인 코드를 업데이트 했습니다. 자세한 사항은 이슈 내 Todo 참조 바랍니다.
    - EDA 업데이트 및 preprocess.py 업데이트 했습니다.
    
    [FEAT] ✨  12.19 코드 업데이트  ✨  #44
    
    * [fix] stratified_split 등 업데이트
    
    - MaskSplitByProfileDataset 에도 startified_split 업데이트 했습니다.
    - mean, std 다시 맞춰줬습니다.
    - EDA 로 split 잘 됨을 확인 했습니다.
    - 잔 오류를 해결했습니다.
    
    [FIX] 🛠️  startified_split 업데이트  🛠️  #46
    
    * [feat] EfficientViT 모델 추가
    
    - model.py 에 Efficient ViT 모델을 추가합니다.
    - 잔 코드들을 수정합니다.
    
    [FEAT] ✨ EfficientViT 추가 ✨  #50
    
    ---------
    
    Co-authored-by: 박정민_T6061 <pjm14418@gmail.com>

--- model.py
+++ model.py
@@ -182,7 +182,10 @@ class MFEfficientResNet(nn.Module):
         )
 
         self.age_classifier = nn.Sequential(
-            nn.Linear(2560, 512),
+            nn.Linear(2560, 1024),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(),
+            nn.Linear(1024, 512),
             nn.LeakyReLU(0.1),
             nn.Dropout(),
             nn.Linear(512, age_num_classes)
@@ -228,7 +231,93 @@ class MFEfficientResNet(nn.Module):
             elif isinstance(m, nn.Linear):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='leaky_relu')
                 m.bias.data.zero_()
-      
+                
+                
+class MFEfficient(nn.Module):
+    def __init__(self, num_classes):
+        super(MFEfficientResNet, self).__init__()
+
+        """
+        1. backbone 선택 후 classifier 차원 수 설정
+        2. 모델의 output_dimension 은 num_classes 로 설정
+        """
+        
+        mask_num_classes = int(num_classes // 6)
+        gender_num_classes = int(num_classes // 9)
+        age_num_classes = int(num_classes // 6)
+        
+        # pretrained model -> 각 모델의 마지막 fc layer 를 빼고 차원 수 맞춰주는 작업 필요
+        efficientnet1 = efficientnet_b7(pretrained=True)
+        efficientnet2 = efficientnet_b7(pretrained=True)
+        self.backbone1 = nn.Sequential(*list(efficientnet1.children())[:-1])
+        self.backbone2 = nn.Sequential(*list(efficientnet2.children())[:-1])
+        
+        # # Freeze pretrained weights
+        # for param in self.features.parameters():
+        #     param.requires_grad = False
+        
+        self.mask_classifier = nn.Sequential(
+            nn.Linear(2560, 512),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(),
+            nn.Linear(512, mask_num_classes)
+        )
+        
+        self.gender_classifier = nn.Sequential(
+            nn.Linear(2560, 512),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(),
+            nn.Linear(512, gender_num_classes)
+        )
+
+        self.age_classifier = nn.Sequential(
+            nn.Linear(2560, 1024),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(),
+            nn.Linear(512, age_num_classes)
+        )
+
+        self.initialize_weights(self.mask_classifier)
+        self.initialize_weights(self.gender_classifier)
+        self.initialize_weights(self.age_classifier)
+    
+    def forward(self, x):
+        """
+        1. Mask와 Gender 는 feature 를 공유하고, Age 는 따로 feature를 사용하여 클래스 별로 3개의 output 출력
+        2. feature는 각각 Mask / Gender, Age
+        """
+        # Feature extraction
+        m_features = self.backbone1(x)
+        m_features = m_features.view(m_features.size(0), -1)  # Flatten features
+        
+        ga_features = self.backbone2(x)
+        ga_features = ga_features.view(ga_features.size(0), -1)
+
+        # Task-specific & Multi feature output
+        mask_output = self.mask_classifier(m_features)
+        
+        gender_output = self.gender_classifier(ga_features)
+        age_output = self.age_classifier(ga_features)
+
+
+        return mask_output, gender_output, age_output
+    
+    def initialize_weights(self, model):
+        """
+        He 가중치 초기화
+        """
+        for m in model.modules():
+            if isinstance(m, nn.Conv2d):
+                init.xavier_uniform_(m.weight.data)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='leaky_relu')
+                m.bias.data.zero_()
+
                 
 # Custom Model Template
 class Mixer(nn.Module):
@@ -325,4 +414,92 @@ class Mixer(nn.Module):
                 m.bias.data.zero_()
             elif isinstance(m, nn.Linear):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='leaky_relu')
+                m.bias.data.zero_()
+                
+
+# Custom Model Template
+class EfficientViT(nn.Module):
+    def __init__(self, num_classes):
+        super(EfficientViT, self).__init__()
+
+        """
+        1. backbone 선택 후 classifier 차원 수 설정
+        2. 모델의 output_dimension 은 num_classes 로 설정
+        """
+        
+        mask_num_classes = int(num_classes // 6)
+        gender_num_classes = int(num_classes // 9)
+        age_num_classes = int(num_classes // 6)
+        
+        # pretrained model -> 각 모델의 마지막 fc layer 를 빼고 차원 수 맞춰주는 작업 필요
+        effvit = timm.create_model('efficientvit_b3.r224_in1k', pretrained=True)
+        efficientnet = efficientnet_b7(pretrained=True)
+        self.backbone1 = nn.Sequential(*list(efficientnet.children())[:-1])
+        self.backbone2 = nn.Sequential(*list(effvit.children())[:-1],
+                                        effvit.head.in_conv,
+                                        effvit.head.global_pool,)
+        
+        # # Freeze pretrained weights
+        # for param in self.features.parameters():
+        #     param.requires_grad = False
+        
+        # three classifier      
+        self.mask_classifier = nn.Sequential(
+            nn.Linear(2560, 512),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(),
+            nn.Linear(512, mask_num_classes)
+        )
+        
+        self.gender_classifier = nn.Sequential(
+            nn.Linear(2304, 2560, bias=False),
+            nn.LayerNorm((2560,),1e-05,elementwise_affine=True),
+            nn.Hardswish(),
+            nn.Dropout(p=0.0, inplace=False),
+            nn.Linear(2560, gender_num_classes, bias=True)
+        )
+
+        self.age_classifier = nn.Sequential(
+            nn.Linear(2304, 2560, bias=False),
+            nn.LayerNorm((2560,),1e-05,elementwise_affine=True),
+            nn.Hardswish(),
+            nn.Dropout(p=0.0, inplace=False),
+            nn.Linear(2560, age_num_classes, bias=True)
+        )
+
+        self.initialize_weights(self.mask_classifier)
+        self.initialize_weights(self.gender_classifier)
+        self.initialize_weights(self.age_classifier)
+    
+    def forward(self, x):
+        """
+        1. Mask와 Gender 는 feature 를 공유하고, Age 는 따로 feature를 사용하여 클래스 별로 3개의 output 출력
+        """
+        # Feature extraction
+        m_features = self.backbone1(x)
+        m_features = m_features.view(m_features.size(0), -1)  # Flatten features
+        
+        ga_features = self.backbone2(x)
+        #ga_features = ga_features.view(ga_features.size(0), -1)
+
+        # Task-specific & Multi feature output
+        mask_output = self.mask_classifier(m_features)
+        
+        gender_output = self.gender_classifier(ga_features)
+        age_output = self.age_classifier(ga_features)
+
+
+        return mask_output, gender_output, age_output
+    
+    def initialize_weights(self, model):
+        """
+        He 가중치 초기화
+        """
+        for m in model.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='leaky_relu')
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
                 m.bias.data.zero_()
